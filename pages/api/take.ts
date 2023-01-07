79486994134e965703dc3b70c9499a554c44c430
thomas <thomas.meerpohl@gmail.com>
2023-01-07 13:43:03 +0100

    Added prettier

--- pages/api/take.ts
+++ pages/api/take.ts
@@ -1,63 +1,60 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Client } from "@notionhq/client";
-import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+import { Client } from '@notionhq/client'
+import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
 type Data = {
-  keyNumber: string
-  who: string
+	keyNumber: string
+	who: string
 }
 
 const getPage = async (keyNumber: string) => {
-  const response = await notion.databases.query({
-    database_id: process.env.DATABASE_ID ?? '',
-    filter: {
-      property: 'Key',
-      select: {
-        equals: keyNumber
-      }
-    }
-  })
+	const response = await notion.databases.query({
+		database_id: process.env.DATABASE_ID ?? '',
+		filter: {
+			property: 'Key',
+			select: {
+				equals: keyNumber,
+			},
+		},
+	})
 
-  return response.results?.[0]?.id ?? null
+	return response.results?.[0]?.id ?? null
 }
 
 const updatePage = async (pageId: string, who: string) => {
-  console.log(who)
-  await notion.pages.update({
-    page_id: pageId,
-    properties: {
-      Bearer: {
-        rich_text: [
-          {
-            type: "text",
-            text: {
-              content: who
-            }
-          }
-        ]
-      }
-    }
-  })
+	console.log(who)
+	await notion.pages.update({
+		page_id: pageId,
+		properties: {
+			Bearer: {
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: who,
+						},
+					},
+				],
+			},
+		},
+	})
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    const {keyNumber, who} = JSON.parse(req.body) as Data
-    const pageId = await getPage(keyNumber)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const { keyNumber, who } = JSON.parse(req.body) as Data
+		const pageId = await getPage(keyNumber)
 
-    if(!pageId) {
-      res.status(404).send('Key not found!')
-      return
-    }
+		if (!pageId) {
+			res.status(404).send('Key not found!')
+			return
+		}
 
-    await updatePage(pageId, who)
-    res.status(200).send('Nice')
-  } catch(error) {
-    res.status(500).send(error)
-  }
+		await updatePage(pageId, who)
+		res.status(200).send('Nice')
+	} catch (error) {
+		res.status(500).send(error)
+	}
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,119 +3,120 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import keyImg from '../public/key.png'
-import { GetServerSideProps, NextPage } from 'next' 
+import { GetServerSideProps, NextPage } from 'next'
 import { Toaster, toast } from 'react-hot-toast'
 
 type Props = {
-  keyNumber: string
+	keyNumber: string
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const props: Props = {
-    keyNumber: context.query?.keyNumber as (string | undefined) ?? ''
-  }
+	const props: Props = {
+		keyNumber: (context.query?.keyNumber as string | undefined) ?? '',
+	}
 
-  return {
-    props: {
-      ...props
-    }
-  }
+	return {
+		props: {
+			...props,
+		},
+	}
 }
 
 const Form = () => {
-  return 
+	return
 }
 
 const Home: NextPage<Props> = (props) => {
-  
-  const {keyNumber} = props
-  const [who, setWho] = React.useState('')
-  const [loading, setLoading] = React.useState(false)
-  const [success, setSuccess] = React.useState(false)
+	const { keyNumber } = props
+	const [who, setWho] = React.useState('')
+	const [loading, setLoading] = React.useState(false)
+	const [success, setSuccess] = React.useState(false)
 
-  React.useEffect(() => {
-    setWho(localStorage.getItem('who') ?? '')
-  }, [])
+	React.useEffect(() => {
+		setWho(localStorage.getItem('who') ?? '')
+	}, [])
 
-  const onSend = async () => {
-    try {
-      
-      setLoading(true)
+	const onSend = async () => {
+		try {
+			setLoading(true)
 
-      const promise = fetch('/api/take', {
-        method: 'POST',
-        body: JSON.stringify({
-          keyNumber,
-          who
-        })
-      })
-      
-      await toast.promise(promise, {
-        loading: 'Updating...',
-        success: () => {
-          setSuccess(true)
-          localStorage.setItem('who', who)
-          return 'Updated key value'
-        },
-        error: (error) => error,
-      })
+			const promise = fetch('/api/take', {
+				method: 'POST',
+				body: JSON.stringify({
+					keyNumber,
+					who,
+				}),
+			})
 
-    } finally {
-      setLoading(false)
-    }
-  }
-  
-  return (
-    <>
-      <Head>
-        <title>oka key tracker</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, " />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+			await toast.promise(promise, {
+				loading: 'Updating...',
+				success: () => {
+					setSuccess(true)
+					localStorage.setItem('who', who)
+					return 'Updated key value'
+				},
+				error: (error) => error,
+			})
+		} finally {
+			setLoading(false)
+		}
+	}
 
-      <main className={styles.main}>
+	return (
+		<>
+			<Head>
+				<title>oka key tracker</title>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1, '
+				/>
+				<link
+					rel='icon'
+					href='/favicon.ico'
+				/>
+			</Head>
 
-        <Toaster position='top-center'/>
+			<main className={styles.main}>
+				<Toaster position='top-center' />
 
-        <h1>key trade</h1>
-          
-        <div className={styles.container}>
+				<h1>key trade</h1>
 
-          <div className={styles.image}>
-            <Image src={keyImg} alt="Picture of the key" />
-            <span>{keyNumber}</span>
-          </div>
+				<div className={styles.container}>
+					<div className={styles.image}>
+						<Image
+							src={keyImg}
+							alt='Picture of the key'
+							priority
+						/>
+						<span>{keyNumber}</span>
+					</div>
 
-          <fieldset style={{opacity: success ? 0 : 1}}>
-            <label>Who are you?</label>
-            <input 
-              type='text' 
-              value={who} 
-              onChange={(event) => setWho(event.target.value)} 
-              placeholder='Your name'
-              disabled={loading || success}
-            />
-          </fieldset>
+					<fieldset style={{ opacity: success ? 0 : 1 }}>
+						<label>Who are you?</label>
+						<input
+							type='text'
+							value={who}
+							onChange={(event) => setWho(event.target.value)}
+							placeholder='Your name'
+							disabled={loading || success}
+						/>
+					</fieldset>
 
-          {
-            success 
-              ? <p>Updated!</p>
-              : (
-                <button 
-                  type='button' 
-                  disabled={loading || success || who === '' || keyNumber === ''}
-                  onClick={onSend}
-                >
-                  Take it
-                </button>
-              )
-          }
-
-        </div>
-
-      </main>
-    </>
-  )
+					{success ? (
+						<p>Updated!</p>
+					) : (
+						<button
+							type='button'
+							disabled={loading || success || who === '' || keyNumber === ''}
+							onClick={onSend}
+						>
+							Take it
+						</button>
+					)}
+				</div>
+			</main>
+		</>
+	)
 }
 
-export default Home;
+export default Home
